c65d5558d5373242aca32542f031d64ea70df8f0
Derek Martin <derekmartin1234@gmail.com>
2019-09-25 23:08:26 -0700

    Maybe this will work

---
+++ day3.py
@@ -0,0 +1,3 @@
+print("Today I realize that a Swedish girl whines and the world pays attention.")
+print("She should have stayed on that hot coco powder box.")
+print("Trying again at 11pm to push this")
