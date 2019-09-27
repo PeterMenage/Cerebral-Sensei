ed018365f4271a8ad2de5c3491ff783b2f0a8acc
Peter Menage <peter.menage@cascadeo.com>
2019-09-26 23:06:52 -0700

    Here is what I was talking about

---
+++ DocString.py
@@ -0,0 +1,7 @@
+a = """An instance is a server in the cloud.
+Websites that are interactive are mostly stored on servers.
+They can be instances or physical servers.
+Cloud computing is putting apps/websites onto these virtual servers.
+Also known as instances.
+ """
+print(a)
